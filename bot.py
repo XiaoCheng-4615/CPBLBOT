82131cc72f1a685fd32675cc454f83249762e75b
XiaoCheng-4615 <joe035823706@gmail.com>
2023-04-14 18:24:14 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,6 +22,7 @@ async def on_ready():
     print(f"目前登入身份 --> {bot.user}")
     print(f"已載入 {len(slash_commands)} 個斜線指令")
 
+
 async def status_task():
     # 缓存数据和缓存更新时间
     cached_data = None
@@ -36,7 +37,7 @@ async def status_task():
             schedule_content = requests.get(url)
             soup = BeautifulSoup(schedule_content.text, 'html.parser')
             team = soup.find_all('div', {'class': 'AllGamesList'})
-            
+            time = soup.find_all('td', {'class': 'team_cinter'})
             # 更新缓存数据和缓存更新时间
             cached_data = team
             cache_expiration_time = datetime.datetime.now() + datetime.timedelta(minutes=5)
@@ -44,15 +45,52 @@ async def status_task():
         # 根据缓存数据更新Discord状态
         if len(cached_data) == 0:
             await bot.change_presence(activity=discord.Game(f'今天沒有比賽'), status=discord.Status.online)
+            current_status = 0
+            status_nogame = [
+                {'type': 'Playing', 'name': f'今天日期: {date}'},
+                {'type': 'Listening', 'name': '今天沒有比賽'},
+                {'type': 'Listening', 'name': '⚾中華職棒(非官方) | PHACS 製作'},
+            ]
+            while True:
+                nogame = discord.Game(name=status_nogame[current_status]['name'], type=status_nogame[current_status]['type'])
+                await bot.change_presence(activity=nogame)
+                current_status = (current_status + 1) % len(status_nogame)
+                await asyncio.sleep(10)  # 状态循环时间
         else:
-            await bot.change_presence(activity=discord.Game(f'今天有 {len(cached_data)} 場比賽'), status=discord.Status.online)
-        
-        # 更新日期和其他信息的Discord状态
-        date = datetime.date.today().strftime("%Y%m%d")
-        await bot.change_presence(activity=discord.Game(f'今天日期: {date}'), status=discord.Status.online)
-        await asyncio.sleep(3) # 更改動態的秒數
-        await bot.change_presence(activity=discord.Game(f'⚾中華職棒(非官方) | PHACS 製作'), status=discord.Status.online)
-        await asyncio.sleep(3) # 更改動態的秒數
+            if len(cached_data) > 1:
+                for i, game in enumerate(cached_data):
+                    game_text = game.text.strip()
+                    game_time = time[i].text.strip()  # 每個比賽對應的時間在time變數中出現兩次，所以需要選取對應的那一個
+                current_status = 0
+                status_game = [
+                    {'type': 'Playing', 'name': f'今天日期: {date}'},
+                    {'type': 'Playing', 'name': f'今天有 {len(cached_data)} 場比賽'},
+                    {'type': 'Playing', 'name': f'{cached_data[0].text.strip()}, {game_time}'},
+                    {'type': 'Playing', 'name': f'{cached_data[1].text.strip()}, {game_time}'},
+                    {'type': 'Listening', 'name': '⚾中華職棒(非官方) | PHACS 製作'},
+                ]
+                while True:
+                    game = discord.Game(name=status_game[current_status]['name'], type=status_game[current_status]['type'])
+                    await bot.change_presence(activity=game)
+                    current_status = (current_status + 1) % len(status_game)
+                    await asyncio.sleep(5)  # 状态循环时间
+            else:
+                for i, game in enumerate(cached_data):
+                    game_text = game.text.strip()
+                    game_time = time[i].text.strip()  # 每個比賽對應的時間在time變數中出現兩次，所以需要選取對應的那一個
+                current_status = 0
+                status_game1 = [
+                    {'type': 'Playing', 'name': f'今天日期: {date}'},
+                    {'type': 'Playing', 'name': f'今天有 {len(cached_data)} 場比賽'},
+                    {'type': 'Playing', 'name': f'{cached_data[0].text.strip()}, {game_time}'},
+                    {'type': 'Listening', 'name': '⚾中華職棒(非官方) | PHACS 製作'},
+                ]
+                while True:
+                    game1 = discord.Game(name=status_game1[current_status]['name'], type=status_game[current_status]['type'])
+                    await bot.change_presence(activity=game1)
+                    current_status = (current_status + 1) % len(status_game1)
+                    await asyncio.sleep(5)  # 状态循环时间
+             
 
 # 載入指令程式檔案
 @bot.command()
